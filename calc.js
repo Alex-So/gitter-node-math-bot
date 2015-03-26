7d2a0227a9822a81ffefff5d3526a6edea183a32
Alex-So <fritz333@inbox.ru>
2015-03-27 00:50:15 +0200

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,3 +1,26 @@
-<script "text/javascript">
-alert('Hello');
-</script>
+var https = require('https');
+
+var roomId    = process.env.ROOM_ID;
+var token     = process.env.TOKEN;
+var heartbeat = " \n";
+
+var options = {
+  hostname: 'stream.gitter.im',
+  port:     443,
+  path:     '/v1/rooms/' + roomId + '/chatMessages',
+  method:   'GET',
+  headers:  {'Authorization': 'Bearer ' + token}
+};
+
+var req = https.request(options, function(res) {
+  res.on('data', function(chunk) {
+    var msg = chunk.toString();
+    if (msg !== heartbeat) console.log('Message: ' + msg);
+  });
+});
+
+req.on('error', function(e) {
+  console.log('Something went wrong: ' + e.message);
+});
+
+req.end();
